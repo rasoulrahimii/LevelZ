800fc03e94b45b8144a339b0e48d2a8aedc201b5
Rasoul Rahimi <mr.nevisane@gmail.com>
2024-09-06 05:32:51 +0330

    Define API routes with throttling

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+declare(strict_types=1);
+
+use App\Http\Controllers\RegisterMobileController;
+use App\Http\Controllers\RegisterOnDeviceController;
+use App\Http\Controllers\RegisterVerifyController;
+use App\Http\Controllers\SetPinController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// maximum of 60 requests per minute
+Route::middleware('throttle:60,1')->group(function () {
+    Route::post('/users/register/mobile', RegisterMobileController::class)->name('users.register.mobile');
+    Route::post('/users/register/verify', RegisterVerifyController::class)->name('users.register.verify');
+    Route::post('/users/register/device', RegisterOnDeviceController::class)->name('users.register.device');
+    Route::post('/users/set-pin', SetPinController::class)->name('users.set.pin');
+});
