9d6882f0d0c00196f13c4404725c53320cee8ba4
Rasoul Rahimi <mr.nevisane@gmail.com>
2024-09-06 05:26:05 +0330

    Add full mobile number method to the User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,25 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $country_code
+ * @property string $mobile
+ * @property string|null $pin
+ * @property string|null $token
+ * @property string|null $mobile_verified_at
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -25,16 +35,6 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
@@ -43,7 +43,12 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'mobile_verified_at' => 'datetime',
         ];
+    }
+
+    public function getFullMobileNumber(): string
+    {
+        return $this->country_code.$this->mobile;
     }
 }
